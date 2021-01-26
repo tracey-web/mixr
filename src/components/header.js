7756e5e8bd853c460b6d1099bec4e8ce788e8772
Tracey <tracey@coch.pro>
2021-01-26 23:19:55 +1100

    Header updates for recipies.js page

--- src/components/header.js
+++ src/components/header.js
@@ -4,40 +4,42 @@ import React from "react"
 import { Typography } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   brandHeader: {
-    marginTop: "-10",
+    marginTop: -theme.spacing(4),
   },
-})
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <Typography variant="h1">
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </Typography>
-    </div>
-  </header>
-)
-
+  header: {
+    backgroundColor: theme.palette.primary.main,
+    marginBottom: theme.spacing(2),
+  },
+}))
+const Header = ({ siteTitle }) => {
+  const classes = useStyles()
+  return (
+    <header className={classes.header}>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <Typography className={classes.brandHeader} variant="h1">
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              fontWeight: 700,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </Typography>
+      </div>
+    </header>
+  )
+}
 Header.propTypes = {
   siteTitle: PropTypes.string,
 }
